543da50b60caa96283671e8eb8a7c6d663af3022
Timilehin Alabi <87454677+turnrcode@users.noreply.github.com>
2022-10-14 14:52:21 +0100

    Add files via upload

---
+++ src/components/Button.vue
@@ -0,0 +1,20 @@
+<template>
+<!-- when creating an event you can use v-eventName="functionName()" or @eventName="functionName()" -->
+    <button @click="onClick()" :style="{ background: color }" class="btn">{{ text }}</button>
+</template>
+
+<script>
+    export default {
+        // eslint-disable-next-line
+        name: 'Button',
+        props: {
+            text: String,
+            color: String,
+        },
+        methods: {
+            onClick() {
+                this.$emit('btn-click')
+            },
+        },
+    }
+</script>
---
+++ src/components/Footer.vue
@@ -0,0 +1,19 @@
+<template>
+  <footer>
+    <p>Copyright &copy; Turnr 2022</p>
+    <a href="github.io/turnrcode/task-tracker">About</a>
+    <!-- <router-link to="/about">About</router-link> -->
+  </footer>
+</template>
+
+
+<style scoped>
+a {
+  color: #333;
+}
+
+footer {
+  margin-top: 30px;
+  text-align: center;
+}
+</style>
